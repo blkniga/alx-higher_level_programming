ce4d68efb95269db3dc300403ffb69904bb32a8d
Bulelani Botman <botmanbulelani@gmail.com>
2022-09-20 17:56:56 -0700

    task 9, pt. 2

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    return number % 10
+    if number > 0:
+        answer = number % 10
+    else:
+        answer = number % -10
+    return(answer)
+
