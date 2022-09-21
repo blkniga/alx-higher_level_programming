9b3d30c8a119eab277a8a1f0f57fa3cf22c13242
Bulelani Botman <botmanbulelani@gmail.com>
2022-09-20 18:14:21 -0700

    task 12

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+def fizzbuzz():
+    for i in range(1, 101):
+        if i % 3 == 0:
+            a = "Fizz"
+        elif i % 3 == 0 and i % 5 == 0:
+            a = "FizzBuzz"
+        else:
+            a = i
+    print("{}".format(a), end=" ")
