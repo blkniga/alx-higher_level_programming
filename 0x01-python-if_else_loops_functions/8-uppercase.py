4c64af61201e4f92c02713670b0af13a4d9f89fa
Bulelani Botman <botmanbulelani@gmail.com>
2022-09-20 17:27:07 -0700

    task 8, pt. 4

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
-def uppercase(str):
-    string = ''
-    for c in str:
-        string = string + (chr(c) - 32)
-    return print(string)
+def uppercase(string):
+    s = ''
+    for c in string:
+        s = s + (chr(c) - 32)
+    print("{}".format(s))
