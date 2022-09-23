b3e955ff12b87420e471fb72e9c50cfe97fc5cb1
Bulelani Botman <botmanbulelani@gmail.com>
2022-09-22 17:10:06 -0700

    task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+
+    if len(argv) == 0:
+        print("{} arguments.".format(len(argv)))
+    elif len(argv) == 1:
+        print("{} argument:".format(len(argv)), "{}: {}".format(1, argv[1]))
+    else:
+        print("{} arguments.".format(len(argv)))
+        for i in range(1, len(arg) + 1):
+            print("{}: {}".format(i, argv[i]))
