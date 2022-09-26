a4bcbce9a039cb8821347b82aeebe635b9a26185
Bulelani Botman <botmanbulelani@gmail.com>
2022-09-26 16:46:02 -0700

    task 0, pt. 2

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    def print_list_integer(my_list):
-        for integer in my_list:
-            print("{}".format(integer))
+def print_list_integer(my_list=[]):
+    for integer in my_list:
+        print("{:d}".format(integer))
