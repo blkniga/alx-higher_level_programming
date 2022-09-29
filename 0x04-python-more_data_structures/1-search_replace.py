6d6083cd0160e1981dee5828cf19e9ac837345f4
Bulelani Botman <botmanbulelani@gmail.com>
2022-09-29 16:33:02 -0700

    task 1, pt. 3 (code redo)

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
+    lst = []
     for element in my_list:
+        lst.append(element)
+    for element in lst:
         if element == search:
-            idx = my_list.index(element)
-            my_list[idx] = replace
-    return my_list
+            idx = lst.index(element)
+            lst[idx] = replace
+    return lst
