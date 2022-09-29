e61c8a8dd5461edf017c7790d6ce6163a4329e25
Bulelani Botman <botmanbulelani@gmail.com>
2022-09-29 16:23:30 -0700

    task 1

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    for element in my_list:
+        if element == search:
+            idx = list.index(element)
+            my_list[idx] = replace
+    return my_list
