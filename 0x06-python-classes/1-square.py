11b77519898dcce83ea608a4e127ab68f3a0aa69
Bulelani Botman <botmanbulelani@gmail.com>
2022-10-25 08:05:05 -0700

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This is a module that deals with a Square"""
+
+
+class Square:
+    """This is a class that defines a Square"""
+
+    def __init__(self, size):
+        self._size = size
