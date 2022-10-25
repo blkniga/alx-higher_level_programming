c7411524a1d728e513c0801dc01baabe5f49aad4
Bulelani Botman <botmanbulelani@gmail.com>
2022-10-25 08:11:03 -0700

    task 1, pt. 3

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -6,5 +6,10 @@ class Square:
     """This is a class that defines a Square"""
 
     def __init__(self, size):
-        """This is an initialization method which takes the parameter of size"""
+        """This is an initialization method.
+
+        Args:
+            size: The size parameter.
+
+        """
         self._size = size
