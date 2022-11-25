d6017d552bf043c100bd15bc120763dec4aecdf1
Bulelani Botman <botmanbulelani@gmail.com>
2022-11-24 19:40:05 -0800

    task 2

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This module has one class which has one method which deals with
+sorted elements."""
+
+class MyList(list):
+    """A class with has a one method called print_sorted()"""
+
+    def print_sorted(self):
+        """The method prints the list, sorted in ascending order"""
+        
+        print(sorted(list))
