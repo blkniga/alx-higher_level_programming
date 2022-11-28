13e2af3210851f2a430826130c5c792866c63557
Bulelani Botman <botmanbulelani@gmail.com>
2022-11-28 06:17:00 -0800

    task 7

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+'''This is the module for a class.'''
+
+
+class BaseGeometry:
+    """This is a class on geometry"""
+
+    def area(self):
+        """A method about an area of a class instance"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """The method validates and integer value.
+
+        Args:
+            name: A random string
+            value: The integer value
+        """
+        if value != int:
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be an greater than 0")
+
