069954f811b415c0f2239f609a888c73d65f3689
Bulelani Botman <botmanbulelani@gmail.com>
2022-11-28 05:59:12 -0800

    task 4, pt. w

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -10,7 +10,7 @@ def inherits_from(obj, a_class):
         obj: The object to checked.
         a_class: The class to be checked against
     """
-    if isinstance(obj, a_class):
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
         return True
     else:
         return False
