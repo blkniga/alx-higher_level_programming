3bc4c15542b83dd1843ae70b6097e22529fa983f
Bulelani Botman <botmanbulelani@gmail.com>
2022-11-28 06:06:49 -0800

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''This is the module for a class.'''
+
+
+class BaseGeometry:
+    """This is a class on geometry"""
+
+    def area(self):
+        """A method about an area of a class instance"""
+        raise Exception("area() is not implemented")
