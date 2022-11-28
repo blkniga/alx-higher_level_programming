f5a3940cc11547c403d543549477881e732d5bab
Bulelani Botman <botmanbulelani@gmail.com>
2022-11-28 05:04:31 -0800

    task w

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''This is the module for a method checking instances.'''
+
+
+def is_same_class(obj, a_class):
+    """A method that checks if the object is exactly an instance of
+    a specified class.
+
+    Args:
+        obj: The object to checked.
+        a_class: The class to be checked against
+    """
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
