31d166b0cdc505c65f12245abb5cc89d8b17affe
Bulelani Botman <botmanbulelani@gmail.com>
2022-11-28 05:48:57 -0800

    task 4

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''This is the module for a method checking instances.'''
+
+
+def inherits_from(obj, a_class):
+    """A method that checks if the object is an instance of
+    a class that inherited from the specified class
+
+    Args:
+        obj: The object to checked.
+        a_class: The class to be checked against
+    """
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
