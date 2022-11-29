db3b05a666f715c49ab10ddd1703408204def12e
Bulelani Botman <botmanbulelani@gmail.com>
2022-11-29 14:03:33 -0800

    task 4, pt. 2

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,3 +1,3 @@
 -- A script that creates a table called first table
 CREATE TABLE IF NOT EXISTS first_table;
-CREATE TABLE(id INT, name VARCHAR(256));
+CREATE TABLE first_table(id INT, name VARCHAR(256));
