fd06666c9f7362c568369ac5e232636ecf6e8fa7
Bulelani Botman <botmanbulelani@gmail.com>
2022-11-29 14:40:07 -0800

    task 9

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,7 @@
+-- A script that creates a table second_table & adds multiple rows.
+
+CREATE TABLE second_table (id INT, name VARCHAR(256), score INT);
+INSERT INTO second_table VALUES (1, 'John', 10);
+INSERT INTO second_table VALUES (2, 'Alex', 3);
+INSERT INTO second_table VALUES (3, 'Bob', 14);
+INSERT INTO second_table VALUES (4, 'George', 8);
