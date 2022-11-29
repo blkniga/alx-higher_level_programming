04b5466b579e4383cd520371be8bd5f13a83160f
Bulelani Botman <botmanbulelani@gmail.com>
2022-11-29 15:47:52 -0800

    task 16, pt. 3

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,4 +1,4 @@
 -- A script that lists all records of the table second_table
 -- Results should display the score and the name
 -- Records shoudl be listed by descending score
-SELECT score, name FROM second_table WHERE IS NOT NULL ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
