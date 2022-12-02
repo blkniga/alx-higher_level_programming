a04f460fb01bc0d7010bce271bcbe2b2a47c789a
Bulelani Botman <botmanbulelani@gmail.com>
2022-12-01 19:51:13 -0800

    task 8

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,4 +1,4 @@
 -- A script that creates the database htbn_0d_usa and the table states
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE IF NOT EXISTS TABLE states(id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY, name VARCHAR(256));
+CREATE IF NOT EXISTS TABLE states(id INT AUTO_INCREMENT NOT NULL PRIMARY KEY, name VARCHAR(256));
