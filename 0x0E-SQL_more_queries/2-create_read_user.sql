a729742caaa57956483b25465685b1e96fd7112f
Bulelani Botman <botmanbulelani@gmail.com>
2022-12-01 18:46:34 -0800

    task 2

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,4 @@
+-- A script that creates the database hbtn_0d_2 and the user user_0d_2
+
+IF NOT EXISTS CREATE DATABASE hbtn_0d_2;
+IF NOT EXISTS CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
