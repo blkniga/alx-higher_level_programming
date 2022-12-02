25bc7c33db23a6b697812cf9535ea575ce825646
Bulelani Botman <botmanbulelani@gmail.com>
2022-12-01 19:39:52 -0800

    task 7

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,4 @@
+-- A script that creates the database htbn_0d_usa and table cities
+
+CREATE DATABASE IF NOT EXISTS htbn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities(id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY, state_id INT NOT NULL, name VARCHAR(256) NOT NULL, FOREIGN KEY(state_id) REFERENCES states(id));
