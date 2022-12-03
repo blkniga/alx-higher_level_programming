ae3f7087f0ae439f63dfd22ef1a5fa3792da111b
Bulelani Botman <botmanbulelani@gmail.com>
2022-12-02 17:31:32 -0800

    task 10, pt. w

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -3,5 +3,5 @@
 
 SELECT tv_shows.title, tv_shows_genres.genre_id
 FROM tv_shows INNER JOIN tv_shows_genres
-ON tv_shows.id == tv_shows_genres.genre_id
+ON tv_shows.id=tv_shows_genres.genre_id
 ORDER BY tv_shows.title, tv_shows_genres.genre_id ASC;
