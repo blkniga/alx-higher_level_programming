f152c8c8c210df45ec1802d4cc509d0a8a132fa3
Bulelani Botman <botmanbulelani@gmail.com>
2022-12-02 18:12:42 -0800

    task 9, pt. 3

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,3 +1,7 @@
 -- A script that lists all cities contained in the database hbtn_0d_usa
 
-SELECT cities.id, cities.name, states.name FROM hbtn_0d_usa.cities INNER JOIN hbtn_0d_usa.states ON cities.state_id=states.id ORDER BY cities.id;
+SELECT cities.id, cities.name, states.name
+FROM cities 
+INNER JOIN states
+ON cities.state_id=states.id
+ORDER BY cities.id;
