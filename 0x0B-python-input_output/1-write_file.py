1d927881f1eafa3a43c60c56328add901b3d16e0
Bulelani Botman <botmanbulelani@gmail.com>
2023-01-10 02:49:16 -0800

    task 1, pt. 2

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -3,6 +3,7 @@
 
 
 def write_file(filename="", text=""):
-    '''A function that writes a text file & returns the number of characters written'''
+    '''A function that writes a text file
+    & returns the number of characters written'''
     with open(filename, mode="w", encoding="UTF-8") as file:
         return file.write(text)
