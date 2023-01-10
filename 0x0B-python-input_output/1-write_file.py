50143cccc8616372c28f4e6a1f246d2dc7b03c5f
Bulelani Botman <botmanbulelani@gmail.com>
2023-01-10 02:46:43 -0800

    task 1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This a module that deals with files & input / output operations"""
+
+
+def write_file(filename="", text=""):
+    '''A function that writes a text file & returns the number of characters written'''
+    with open(filename, mode="w", encoding="UTF-8") as file:
+        return file.write(text)
