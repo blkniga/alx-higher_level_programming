96b3cba02e40fc04a3f3d36a8e3565437b68c074
Bulelani Botman <botmanbulelani@gmail.com>
2023-01-10 03:19:00 -0800

    task 8

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This a module that deals with files & input / output operations"""
+
+
+def class_to_json(obj):
+    '''A function that returns the dictionary description with simple data structure
+    for JSON serialzation of an object'''
+    return obj.__dict__
