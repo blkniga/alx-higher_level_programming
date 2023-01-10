7a589ab8f199fdb9b6684ea4eef6c7d1438f894f
Bulelani Botman <botmanbulelani@gmail.com>
2023-01-10 03:41:29 -0800

    task 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""This a module that deals with files & input / output operations"""
+
+
+class Student:
+    '''This is a class named student'''
+
+    def __init__(self, first_name, last_name, age):
+        '''The constructor method'''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        '''A method that retrieves a dictionary representation
+        of a Student instance'''
+
+        if attrs is None:
+            return self.__dict__
+        else:
+            d = {}
+            for k, v in self.__dict__.items():
+                for item in attrs:
+                    if item == k:
+                        d[k] = v
+        return d
+
+    def reload_from_json(self, json):
+        '''A method that replaces all attributes of the Student instance'''
+        for k, v in json.items():
+            setattr(self, k, v)
