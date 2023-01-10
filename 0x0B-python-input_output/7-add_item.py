53ba06bd377c7a8d1f5383689c36b724de80153d
Bulelani Botman <botmanbulelani@gmail.com>
2023-01-10 03:05:22 -0800

    task 7

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""This a module that deals with files & input / output operations"""
+import sys
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+file_name = "add_item.json"
+lst = []
+
+for items in sys.argv[1:]:
+    lst.append(items)
+    save_to_json_file(lst, file_name)
+
+load_from_json_file(file_name)
