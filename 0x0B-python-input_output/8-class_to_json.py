f4ee519312276a3530f7f6ccd5698b91421dd73f
Bulelani Botman <botmanbulelani@gmail.com>
2023-01-10 03:22:46 -0800

    task 8, pt. 2

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -3,6 +3,7 @@
 
 
 def class_to_json(obj):
-    '''A function that returns the dictionary description with simple data structure
+    '''A method that returns the dictionary description
+    with simple data structure
     for JSON serialzation of an object'''
     return obj.__dict__
